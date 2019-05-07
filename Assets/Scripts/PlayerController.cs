df7d19f9f104542c2e30a22929d8b5301a354c9d
Thomas Tran <tran.th.thomas@gmail.com>
2019-05-06 23:31:01 -0400

    Add ground check to PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,14 @@ public class PlayerController : MonoBehaviour {
 	public float moveSpeed;
 	private Rigidbody2D myRigidbody;
 
+	public float jumpSpeed;
+
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+
+	public bool isGrounded;
+
 	// Use this for initialization
 	void Start () {
 		myRigidbody = GetComponent<Rigidbody2D>();
@@ -14,6 +22,9 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
 		if (Input.GetAxisRaw ("Horizontal") > 0f)
 		{
 			myRigidbody.velocity = new Vector3 (moveSpeed, myRigidbody.velocity.y, 0f);
@@ -23,6 +34,11 @@ public class PlayerController : MonoBehaviour {
 		} else 
 		{
 			myRigidbody.velocity = new Vector3 (0f, myRigidbody.velocity.y, 0f);
+		}
+
+		if (Input.GetButtonDown ("Jump") && isGrounded)
+		{
+			myRigidbody.velocity = new Vector3 (myRigidbody.velocity.x, jumpSpeed, 0f);
 		}
 	}
 }
