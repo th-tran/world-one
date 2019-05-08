1b0337914b5cb4b91d7972e5e434c1974f0bbcc2
Thomas Tran <tran.th.thomas@gmail.com>
2019-05-07 22:31:46 -0400

    Hold down space to jump higher!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,37 +4,69 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	// Player body and forces
 	public float moveSpeed;
-	public float jumpSpeed;
+	public float jumpForce;
 	public float moveInput;
 	private Rigidbody2D myRigidbody;
 
-	public Transform groundCheck;
-	public float groundCheckRadius;
+	// Controls jumping
+	private float jumpTimeCounter;
+	public float jumpTime;
+	private bool isJumping;
+
+	// Ground check
+	public Transform feetPos;
+	public float checkRadius;
 	public LayerMask whatIsGround;
+	private bool isGrounded;
 
-	public bool isGrounded;
-
+	// Controls player animations
 	private Animator myAnim;
 
 	void Start () {
+		// Get the rigidbody and animator of player
 		myRigidbody = GetComponent<Rigidbody2D>();
 		myAnim = GetComponent<Animator>();
 	}
 	
-	void FixedUpdate () {
+	void Update () {
+		// Move player left or right (or not at all) based on input
         moveInput = Input.GetAxisRaw ("Horizontal");
 		myRigidbody.velocity = new Vector2 (moveInput * moveSpeed, myRigidbody.velocity.y);
 		if (moveInput != 0f) {
+			// Turn the direction the player is facing based on input
 			transform.localScale = new Vector2 (moveInput * 1f, 1f);
 		}
 
-		isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+		// Allow jumping only if player jumps from ground
+		isGrounded = Physics2D.OverlapCircle (feetPos.position, checkRadius, whatIsGround);
 		if (Input.GetButtonDown ("Jump") && isGrounded)
 		{
-			myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpSpeed);
+			isJumping = true;
+			jumpTimeCounter = jumpTime;
+			myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce);
 		}
 
+		// As long as player holds jump button
+		if (Input.GetButton ("Jump") && isJumping == true) 
+		{
+			// If player hasn't run out of jump juice, keep going
+			if (jumpTimeCounter > 0) 
+			{
+				myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce);
+				jumpTimeCounter -= Time.deltaTime;
+			} else { // Player ran out of jump juice
+				isJumping = false;
+			}
+		}
+
+		// Player can only jump once
+		if (Input.GetButtonUp ("Jump")) {
+			isJumping = false;
+		}
+
+		// Set animations based on player values
 		myAnim.SetFloat ("Speed", Mathf.Abs (myRigidbody.velocity.x));
 		myAnim.SetBool ("Grounded", isGrounded);
 	}
