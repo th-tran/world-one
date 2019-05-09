e889f12e45f7033f6d69f939d14f69310ef93819
Thomas Tran <tran.th.thomas@gmail.com>
2019-05-08 21:45:05 -0400

    Add script to remove zombie objects

---
+++ Assets/Scripts/DestroyOverTime.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOverTime : MonoBehaviour {
+
+	public float lifeTime;
+
+	void Update () {
+		lifeTime = lifeTime - Time.deltaTime;
+
+		if (lifeTime <= 0f)
+		{
+			Destroy (gameObject);
+		}
+	}
+}
