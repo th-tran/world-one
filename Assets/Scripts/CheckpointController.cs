9d70d0c7a88cb4e064fcfec1a344af2825939798
Thomas Tran <tran.th.thomas@gmail.com>
2019-05-09 19:29:00 -0400

    Fix bug that allowed Player to return to previously activated checkpoints

--- Assets/Scripts/CheckpointController.cs
+++ Assets/Scripts/CheckpointController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class CheckpointController : MonoBehaviour {
 	// Controls checkpoint animations
 	private Animator checkpointAnim;
+	private CircleCollider2D checkpointCollider;
 	public bool checkpointActive;
 
 	void Start () {
 		checkpointAnim = GetComponent<Animator>();
+		checkpointCollider = GetComponent<CircleCollider2D>();
 		checkpointActive = false;
 	}
 
@@ -20,6 +22,7 @@ public class CheckpointController : MonoBehaviour {
 		if (other.tag == "Player")
 		{
 			checkpointActive = true;
+			checkpointCollider.enabled = false;
 		}
 	}
 }
