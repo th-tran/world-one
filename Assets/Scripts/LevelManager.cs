46796ad1569c227fe492efc441e77237bdb340af
Thomas Tran <tran.th.thomas@gmail.com>
2019-05-08 20:58:11 -0400

    Refactor respawn into coroutine

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour {
 
 	public PlayerController thePlayer;
+	public float waitForRespawn;
 
 	void Start () {
 		thePlayer = FindObjectOfType<PlayerController>();
 	}
 
 	public void Respawn () {
+		// Separate respawn into its own event while time continues
+		StartCoroutine("RespawnCo");
+	}
+
+	public IEnumerator RespawnCo () {
+		// Deactivate player
 		thePlayer.gameObject.SetActive (false);
+
+		// Pause between death and respawn
+		yield return new WaitForSeconds (waitForRespawn);
+
+		// Re-activate player
 		thePlayer.transform.position = thePlayer.respawnPosition;
 		thePlayer.gameObject.SetActive (true);
 	}
