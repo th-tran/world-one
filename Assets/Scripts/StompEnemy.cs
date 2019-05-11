21096fef404822bda55970635eafdd742c862471
Thomas Tran <tran.th.thomas@gmail.com>
2019-05-10 20:28:59 -0400

    Player can bounce on enemies mario style

---
+++ Assets/Scripts/StompEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StompEnemy : MonoBehaviour {
+
+	private Rigidbody2D playerRigidbody;
+
+	public GameObject deathSplosion;
+	public float bounceForce;
+
+	void Start () {
+		playerRigidbody = transform.parent.GetComponent<Rigidbody2D>();
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.tag == "Enemy")
+		{
+			Destroy (other.gameObject);
+			//Instantiate (deathSplosion, other.transform.position, other.transform.rotation);
+			playerRigidbody.velocity = new Vector2 (playerRigidbody.velocity.x, bounceForce);
+		}
+	}
+}
